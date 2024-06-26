4eb350f29def68837633709e76ea0af83008ef15
eniola <eniolafaithful57@gmail.com>
2024-06-26 05:00:26 +0100

    Feat: add login and pprofile page

---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import RegisterForm from '@/app/components/RegisterForm'
+
+const Login = () => {
+  return (
+    <div>
+      <RegisterForm />
+    </div>
+  )
+}
+
+export default Login
