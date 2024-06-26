a6ee843ed64e0dd23bea280cb30bbb0a99a1df46
eniola <eniolafaithful57@gmail.com>
2024-06-26 04:57:05 +0100

    Feat: Implement inbox and parent route

---
+++ app/[id]/inbox/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Inbox = () => {
+  return (
+    <section className='w-full pt-7 px-8 bg-[#151515] h-full'>
+      <div className='flex justify-between items-center'>
+        <div className='font-bold text-2xl'>Inbox</div>
+        <button className='bg-[#06D440] p-[8px_16px] rounded-[4px]'>Create new Inbox</button>
+      </div>
+    </section>
+  )
+}
+
+export default Inbox
