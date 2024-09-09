077cd1ce53b1895964a30298839d14ed3f05a5f0
eniola <eniolafaithful57@gmail.com>
2024-09-09 15:45:38 +0100

    feat: improve layout

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -7,12 +7,12 @@ type Prop = Readonly<{
 
 export default function UserLayout({ children }: Prop) {
   return (
-    <main className="w-full">
+    <main className="w-full min-h-screen">
       <div className="bg-[#080809] w-full fixed top-0 right-0 left-0 h-16"></div>
       <div className="w-[52px] mt-[4rem] fixed min-h-[calc(100vh-49px)] bg-[#FEFEFE08]">
         <NavButtons />
       </div>
-      <div className="ml-[3.7rem]  pt-[4rem]">{children}</div>
+      <div className="ml-[3.7rem] h-[100vh] pt-[4rem]">{children}</div>
     </main>
   );
 }
