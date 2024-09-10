5ea15b2d5693265dc7fa2f347710760dda22f703
eniola <eniolafaithful57@gmail.com>
2024-09-10 16:39:50 +0100

    feat: fetching user data information from the database

--- app/(user)/profile/page.tsx
+++ app/(user)/profile/page.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ProfileContent from "@/components/Profile/ProfileContent";
+import { verifySession } from "@/lib/dal";
+import { base_url } from "@/lib/utils";
 
-const Profile = () => {
+const Profile = async () => {
+  const { userId } = await verifySession();
+  const { username, email } = await fetch(`${base_url}/users/${userId}`)
+    .then((res) => res.json())
+    .then((data) => ({ username: data.username, email: data.email }));
   return (
     <div className="h-[calc(100vh-57px)]">
-      <ProfileContent />
+      <ProfileContent username={username} email={email} />
     </div>
   );
 };
