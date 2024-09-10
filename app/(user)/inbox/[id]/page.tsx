d798739eefb4830762bd24a974f97913250dcb33
eniola <eniolafaithful57@gmail.com>
2024-09-10 16:39:07 +0100

    feat: implement fetching of an inbox

--- app/(user)/inbox/[id]/page.tsx
+++ app/(user)/inbox/[id]/page.tsx
@@ -1,12 +1,24 @@
 import InboxMessages from "@/components/Inbox/InboxMessages";
+import { verifySession } from "@/lib/dal";
 import { base_url } from "@/lib/utils";
 
 async function Inbox({ params }: { params: { id: string } }) {
-  const inbox = await fetch(`${base_url}/inboxes/${params.id}/messages`);
+  const { userId } = await verifySession();
+  const inbox = await fetch(`${base_url}/inboxes/${params.id}/messages`, {
+    method: "GET", // HTTP method
+    headers: {
+      "User-Id": userId.toString(), // Convert userId to string
+      "Content-Type": "application/json", // Optional: If you are expecting JSON response
+    },
+  });
   const inboxMessages = await inbox.json();
+
+  const userInbox = await fetch(`${base_url}/inboxes/${params.id}`)
+    .then((res) => res.json())
+    .then((data) => data);
   return (
     <div>
-      <InboxMessages inboxMessages={inboxMessages} />
+      <InboxMessages inboxName={userInbox.name} inboxMessages={inboxMessages} />
     </div>
   );
 }
