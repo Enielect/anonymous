32be364545aa846a3669a68ee3d77dccbef7e61e
eniola <eniolafaithful57@gmail.com>
2024-09-17 07:31:58 +0100

    feat: try to improve seo.

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,51 @@ import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const description = "Send anonymous messages among friends, foes, and family";
+
 export const metadata: Metadata = {
   title: "Whispers",
-  description: "Send anonymous messages among friends, foes, and family",
+  description: description,
+  applicationName: "whispers",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: "whispers",
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  manifest: "/manifest.json",
+  icons: {
+    apple: "/apple-touch-icon.png",
+    icon: "/favicon.ico",
+  },
+  twitter: {
+    card: "summary",
+    // url: 'https://beastmood.vercel.app',
+    title: "whispers",
+    description: description,
+    images: ["/android-chrome-192x192.png"],
+    creator: "@_whisper",
+  },
+  openGraph: {
+    type: "website",
+    title: "whispers",
+    description: description,
+    siteName: "whispers",
+    url: "https://whispers-gray.vercel.app",
+    images: [
+      {
+        url: "/apple-touch-icon.png",
+      },
+    ],
+  },
+  other: {
+    "mobile-web-app-capable": "yes",
+    "msapplication-config": "/browserconfig.xml",
+    "msapplication-TileColor": "#2B5797",
+    "msapplication-tap-highlight": "no",
+  },
 };
 
 export const viewport: Viewport = {
